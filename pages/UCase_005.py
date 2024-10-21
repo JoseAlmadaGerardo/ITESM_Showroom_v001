22dd14902c625b7b3565e51ded5317e920f926a3
JoseAlmadaGerardo <118389948+JoseAlmadaGerardo@users.noreply.github.com>
2024-10-20 19:14:38 -0700

    Update UCase_005.py

--- pages/UCase_005.py
+++ pages/UCase_005.py
@@ -12,6 +12,8 @@ st.subheader("Factory Asset Effectiveness.​")
 st.write("📄 Answers to questions about .TX, .MD, and .PDF documents. Upload a document below and ask a question about it – GPT will answer!")
 st.write("Note: To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys).")
 
+# Access the OpenAI API key from secrets
+api_key = st.secrets["general"]["OpenAI_key"]
 
 # Allow the user to upload a file
 uploaded_file = st.file_uploader("Upload a document (.txt or .md)", type=("txt", "md"))
