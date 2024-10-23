a4915fb8bb083d0ccbab1def272b82c3a0f15d7c
JoseAlmadaGerardo <A01794415@tec.mx>
2024-10-23 00:26:12 -0700

    Format update
    Files have been renamed to improve the visualization at the app.

--- itesm_showroom.py
+++ itesm_showroom.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Set page configuration
-st.set_page_config(page_title="Hello", page_icon="👋")
+st.set_page_config(page_title="Hello", page_icon="👋", layout="wide")
 
 # Load the API key from secrets
 if "api_key" not in st.session_state:
@@ -10,23 +10,44 @@ if "api_key" not in st.session_state:
 st.write("# Welcome to the Data Science Hub Showroom at Tecnológico de Monterrey! 👋")
 st.sidebar.success("Select a demo above.")
 
+# Use the full page instead of a narrow central column
 
 st.markdown(
-        """This AI showroom showcases various use cases across different industrial applications. It is an open-source app built on the Streamlit framework, this showroom is specifically designed for AI agents utilizing RAG architecture from OpenAI.
+        """
+        ### AI Use Cases
+        This showroom showcases various AI use cases across different industries applications, and is an app built
+        on the Streamlit framework specifically designed for AI agents utilizing RAG architecture from OpenAI.
 
-👈 Select a demo from the dropdown on the left to explore examples of what AI assistance can achieve!
+        👈 Select a demo from the dropdown on the left to explore examples of what AI assistance can achieve!
+        """
+    )
 
-        ### Want to learn more about streamlit?
 
+# Create 2 columns
+col1, col2 = st.columns((1.5,1))
+
+with col1:
+    st.markdown(
+        """
+        ## Objetives.
+        Analyze different industrial sectors to identify potential opportunities
+        where it is feasible to develop AI-based assistants using RAG architecture.
+        The industries to explore are the next, Accounting & Tax, Industry 4.0,
+        Finance, Marketing and Business Units.
+        
+        These assistants will be able to incorporate personalized knowledge & integrate
+        call functions!
+        """
+    )
+
+with col2:
+    st.markdown(
+        """
+        ### Learn About Streamlit, Assistant API and More Demos
         - Check out [streamlit.io](https://streamlit.io)
         - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-
-        ### Want to learn more anout Assistant API use at this project?
-
-        - Check out [AssistanAPI.openAI](https://platform.openai.com/docs/assistants/overview)
-
-        ### See more complex demos
-    """
+        - Ask a question in our [community forums](https://discuss.streamlit.io)
+        - Check out [Assistant API by OpenAI](https://platform.openai.com/docs/assistants/overview)
+        - Explore additional complex demos and industry use cases.
+        """
     )
