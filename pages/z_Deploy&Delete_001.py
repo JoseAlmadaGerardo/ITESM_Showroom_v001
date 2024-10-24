b45e851aa4fac8357a34a25a4681cf3efafc4023
JoseAlmadaGerardo <118389948+JoseAlmadaGerardo@users.noreply.github.com>
2024-10-24 13:41:02 -0700

    Update z_Deploy&Delete_001.py

--- pages/z_Deploy&Delete_001.py
+++ pages/z_Deploy&Delete_001.py
@@ -6,11 +6,10 @@ from urllib.error import URLError
 #from openai import OpenAI
 
 st.set_page_config(page_title="UCase_003", page_icon="📊")
-st.markdown("# UCase_003")
-st.sidebar.header("UCase_003")
+st.markdown("# template_001")
+st.sidebar.header("Template")
 
-# Title.
-st.title("Manufacturing: Use Case #3​")
+st.title("Just a template")
 st.subheader("Factory Asset Effectiveness.​")
 st.write("📄 Answers to questions about .TX, .MD, and .PDF documents. Upload a document below and ask a question about it – GPT will answer!")
 st.write("Note: To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys)."
