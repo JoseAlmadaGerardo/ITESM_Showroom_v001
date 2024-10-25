e3988bc9c15f00b6ce202b8b4f9b4097f2af7cc0
JoseAlmadaGerardo <118389948+JoseAlmadaGerardo@users.noreply.github.com>
2024-10-25 10:53:06 -0700

    Update A-Accounting&Taxes .py

--- pages/A-Accounting&Taxes .py
+++ pages/A-Accounting&Taxes .py
@@ -43,38 +43,37 @@ def data_entry_conversion():
         """
     )
     st.write("More details about Data Entry to Invoice Conversion will be added here.")
-
+# Modificacion
 # Main Page Selection
 page_names_to_funcs = {
     "—": lambda: st.write("Select a page from the sidebar."),
-    "📄 Accounting Records Tracking": accounting_records_tracking,
-    "📄 Accounting Calculations": accounting_calculations,
-    "📄 Conversión de entrada de datos en facturas basadas en imágenes": data_entry_conversion,
+    "📄 Accounting records tracking": accounting_records_tracking,
+    "📄 Accounting calculations": accounting_calculations,
+    "📄 Data entry conversion": data_entry_conversion,
 }
 
 # Sidebar for Navigation
-st.sidebar.header("AI Applications in Accounting and Taxes")
-demo_name = st.sidebar.selectbox("Choose a use case", page_names_to_funcs.keys())
-
-# Render Selected Page
-page_names_to_funcs[demo_name]()
-
-# General Progress Bar and Placeholder (can be reused across pages)
-#INTRO:
-st.markdown("# AI applications at Accounting and taxes.")
 st.sidebar.header("AI APPLICATIONS AT ACCOUNTING AND TAXES.")
-st.markdown(
+demo_name = st.sidebar.selectbox("Choose a use case", page_names_to_funcs.keys())
+st.markdown("# AI applications at Accounting and taxes.")
+
+# Render Main Introductory Content Only on Main Page
+if demo_name == "—":
+    st.markdown(
     """
     Accounting, finance, and taxation in Mexico are critical areas that demand accuracy 
     and constant scrutiny. Confirming accounting records is essential to ensure that all 
     financial transactions are properly documented and reflect the entity's economic reality. 
     Additionally, accounting calculations such as depreciation and amortization help distribute 
     asset costs over time, providing a more accurate view of their value and utility.
-    """
-)
+        
+        **Explore Use Cases:**
+        - 📄 Accounting records tracking.
+        - 📄 Accounting calculations (depreciation, amortization, among others.
+        - 📄 Conversión de entrada de datos en facturas basadas en imágenes.
+        """
+    )
+    st.write("👈 Select a demo from the dropdown on the left to explore examples of what AI assistance can achieve!")
 
-st.title("Uses case at Accounting and Taxes")
-st.write(" - 📄 Accounting records tracking.")
-st.write(" - 📄 Accounting calculations (depreciation, amortization, among others).")
-st.write (" - 📄 Conversión de entrada de datos en facturas basadas en imágenes.")
-st.write("👈 Select a page from the sidebar to explore AI use cases in Accounting.")
+# Render Selected Page
+page_names_to_funcs[demo_name]()
